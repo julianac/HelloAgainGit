c916653656050559ca2b12c213228cd7d56de6aa
Juliana Cipa <cipa_j@hotmail.com>
2012-01-25 15:56:45 +0000

    fixed the changes.

--- src/com/peerius/project2/P2test2.java
+++ src/com/peerius/project2/P2test2.java
@@ -10,23 +10,24 @@ public class P2test2
 	//this is the useless empty constructor
 	public P2test2()
 	{
-		final int useless = 88;
+		final int useless = 80008;
 		final int useless2 = 55;
 		final int a = 2;
 		final float testing3 = 2;
-		final int b = 5;
 	}
 
 	public void hello()
 	{
+
 	}
 
 	public void hello3()
 	{
+
 	}
 
-	public void emptyMethod()
+	public void hello2()
 	{
-		int b = 0;
+
 	}
 }
