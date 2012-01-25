2198d5c4ab2374b7ca739b5d026642de47985a64
Gian Mario Contessa <gianmario.contessa@peerius.com>
2012-01-25 15:38:07 +0000

    committing

--- src/com/peerius/project2/P2test2.java
+++ src/com/peerius/project2/P2test2.java
@@ -10,14 +10,18 @@ public class P2test2
 	//this is the useless empty constructor
 	public P2test2()
 	{
-		final int useless = 88;
+		final int useless = 80008;
 		final int useless2 = 55;
 		final int a = 2;
 		final float testing3 = 2;
-		final int b = 5;
 	}
 
 	public void hello()
+	{
+
+	}
+
+	public void hello2()
 	{
 
 	}
